bf595d59210b24e94f41e5b0470a026a3829bcbd
aylin2708 <aylin2127maria@gmail.com>
2021-08-11 20:05:53 -0300

    shorten url

---
+++ bot3.py
@@ -0,0 +1,81 @@
+import pyshorteners
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
+from telegram import ChatAction, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+INPUT_URL = 0
+
+
+def start(update, context):
+
+    update.message.reply_text(
+        text='¡Hola bienvenido!, ¿qué deseas hacer?',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Acortar URL', callback_data='url')],
+        ])
+    )
+
+def qr_command_handler(update, context):
+
+    update.message.reply_text('Envíe el texto para generar un código QR')
+
+    return INPUT_URL
+
+def url_callback_handler    (update, context):
+
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text(
+        text='Envíe un enlace para acortarlo'
+    )
+
+    return INPUT_URL
+
+def input_url(update, context):
+
+    url = update.message.text
+    chat = update.message.chat
+
+    #acortar url
+
+    s = pyshorteners.Shortener()
+
+    short = s.chilpit.short(url)
+    'http://chilp.it/TEST'
+
+
+    chat.send_action(
+        action=ChatAction.TYPING,
+        timeout=None
+    )
+
+    chat.send_message(
+        text=short
+    )
+
+    
+    return ConversationHandler.END
+
+if __name__ == '__main__':
+
+    updater = Updater(token='1949314508:AAGDpitaLVzXmNFMIeugNvELx9kDNzjHJeM', use_context=True)
+    
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            CallbackQueryHandler(pattern='url', callback=url_callback_handler)   
+        ],
+
+        states={
+            INPUT_URL: [MessageHandler(Filters.text, input_url)]
+        },
+
+        fallbacks=[]
+    ))
+
+    updater.start_polling()
+    updater.idle()
