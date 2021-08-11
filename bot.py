32e77f6c7d7df962562fd6525542cd06426f1f62
aylin2708 <aylin2127maria@gmail.com>
2021-08-11 00:39:46 -0300

    funcionalidades qr

--- bot.py
+++ bot.py
@@ -1,8 +1,40 @@
-from telegram.ext import Updater, CommandHandler, updater
+import qrcode
+
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+
+INPUT_TEXT = 0
 
 def start(update, context):
 
-    update.message.reply_text('Hello, human!')
+    update.message.reply_text('¡Hola bienvenido!, ¿qué deseas hacer?\n\nUsa /qr para generar un código qr')
+
+def qr_command_handdler(update, context):
+
+    update.message.reply_text('Envíe el texto para generar un código QR')
+
+    return INPUT_TEXT
+
+def generate_qr(text):
+
+    filename = text + '.jpg'
+
+    img = qrcode.make(text)
+    img.save(filename)
+
+    return filename
+
+
+def input_text(update, context):
+
+    text = update.message.text
+
+    filename = generate_qr(text)
+
+    print(filename)
+    
+    #send_qr(filename)
+
+    return ConversationHandler.END
 
 if __name__ == '__main__':
 
@@ -11,6 +43,18 @@ if __name__ == '__main__':
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
+
+    dp.add_handler(ConversationHandler(
+        entry_points=[
+            CommandHandler('qr', qr_command_handdler)
+        ],
+
+        states={
+            INPUT_TEXT: [MessageHandler(Filters.text, input_text)]
+        },
+
+        fallbacks=[]
+    ))
 
     updater.start_polling()
     updater.idle()
